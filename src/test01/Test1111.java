2898ec4108439ee7c16e5f5aef17afa583935898
linchun <1004450751@qq.com>
2017-03-20 00:42:16 +0800

    获得过几个

---
+++ src/test01/Test1111.java
@@ -0,0 +1,24 @@
+package test01;
+
+public class Test1111 {
+
+	public static void main(String[] args) {
+   System.out.println(decision());
+	}
+
+	static boolean decision() {
+		try {
+			if (true)
+				return true;
+
+			return true;
+
+		} finally {
+			if (true)
+				return true;
+
+			return true;
+
+		}
+	}
+}
